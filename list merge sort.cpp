728817439ec8f4269ad068dc96c9e036efc39ff4
DaryaPanyukova <panyukova.darya@bk.ru>
2021-05-23 11:46:16 +0300

    init

---
+++ list merge sort.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+
+
+struct node {
+    int data;
+    node* next;
+    node* prev;
+};
+
+node* head = nullptr;
+
+void insert(int x) {
+    node* q = new node;
+    q->data = x;
+    q->prev = nullptr;
+    q->next = head;
+
+    if (head != nullptr)
+        head->prev = q;
+    head = q;    
+}
+
+void PrintList(node* L) {
+    node* p = L;
+    while (p != nullptr) {
+        std::cout << p->data << ' ';
+        p = p->next;
+    }
+    std::cout << '\n';
+}
+
+void MakeLists(node* ptr_lhs, node* &ptr_rhs) {
+    ptr_lhs->prev = nullptr;
+    node* tmp_lhs = ptr_lhs;
+    node* tmp_rhs = ptr_rhs;
+
+    while (tmp_lhs != nullptr) {
+        tmp_lhs = tmp_lhs->next;
+        if (tmp_lhs != nullptr) {
+            tmp_rhs = tmp_rhs->next;
+            tmp_lhs = tmp_lhs->next;
+        }
+    }
+
+    if (tmp_rhs->prev != nullptr) {
+        tmp_rhs->prev->next = nullptr;
+        tmp_rhs->prev = nullptr;
+    }
+
+    ptr_rhs = tmp_rhs;
+}
+
+void tie(node* &lhs, node* rhs, node* end) {
+    if (lhs == nullptr) {
+        lhs = rhs;
+        return;
+    }
+    end->next = rhs;
+    rhs->prev = lhs;
+}
+
+void Merge(node* &lhs, node* &rhs) {
+    node* res = nullptr;
+    node* end = nullptr;
+
+    while (lhs != nullptr && rhs != nullptr) {
+        if (lhs->data < rhs->data) {
+            node* tmp = lhs->next;
+            lhs->next = nullptr;
+            tie(res, lhs, end);
+            end = lhs;
+            lhs = tmp;
+        } else {
+            node* tmp = rhs->next;
+            rhs->next = nullptr;
+            tie(res, rhs, end);
+            end = rhs;
+            rhs = tmp;
+        }
+    }
+
+    if (lhs != nullptr) {
+        tie(res, lhs, end);
+    }
+    if (rhs != nullptr) {
+        tie(res, rhs, end);
+    }
+
+    lhs = res;
+}
+
+void MergeSort(node*& ptr) {
+    if (ptr == nullptr || ptr->next == nullptr) {
+        return;
+    }
+    node* ptr_lhs = ptr;
+    node* ptr_rhs = ptr;
+    MakeLists(ptr_lhs, ptr_rhs);
+
+    MergeSort(ptr_lhs);
+    MergeSort(ptr_rhs);
+
+    Merge(ptr_lhs, ptr_rhs);
+    ptr = ptr_lhs;
+}
+
+int main()
+{
+
+    int size_of_array;
+    std::cin >> size_of_array;
+
+    for (int i = 0; i < size_of_array; ++i) {
+        int A = rand();
+        insert(A);
+    }
+
+    PrintList(head);
+    MergeSort(head);
+    PrintList(head);
+    std::cout << '\n';
+}
---
+++ list quick sort.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+
+
+
+struct node {
+    int data;
+    node* next;
+};
+
+node* head = nullptr;
+
+void insert(node* &L, int x) {
+    node* q = new node;
+    q->data = x;
+    q->next = L;
+    L = q;
+}
+
+void PrintList(node *L) {
+    node* p = L;
+    while (p != nullptr) {
+        std::cout << p->data << ' ';
+        p = p->next;
+    }
+    std::cout << '\n';
+}
+
+void tie(node* &lhs, node* rhs) {
+    node* ptr = lhs;
+    if (lhs == nullptr) {
+        lhs = rhs;
+        return;
+    }
+    while (ptr->next != nullptr) {
+        ptr = ptr->next;
+    }
+    ptr->next = rhs;
+}
+
+void QuickSort(node *&lhs) {
+    if (lhs != nullptr) {
+        if (lhs->next == nullptr) return;
+        int pivot = lhs->data;
+
+        node* lhs_beg = nullptr;
+        node* mid_beg = nullptr;
+        node* rhs_beg = nullptr;
+
+        node* p = lhs;
+           
+        while (p != nullptr) {
+            node* tmp = p->next;
+            if (p->data < pivot) {
+                p->next = nullptr;
+                tie(lhs_beg, p);
+                p = tmp;
+            } else if (p->data == pivot) {
+                p->next = nullptr;
+                tie(mid_beg, p);
+                p = tmp;
+            } else {
+                p->next = nullptr;
+                tie(rhs_beg, p);
+                p = tmp;
+            }
+        }
+
+        QuickSort(lhs_beg);
+        QuickSort(rhs_beg);
+
+        tie(lhs_beg, mid_beg);
+        tie(lhs_beg, rhs_beg);
+
+        lhs = lhs_beg;
+        if (lhs == head) {
+            head = lhs_beg;
+            return;
+        }
+    }
+}
+
+int main()
+{
+    int size_of_array;
+    std::cin >> size_of_array;
+
+    for (int i = 0; i < size_of_array; ++i) {
+        int A = rand();
+        insert(head, A);
+    }
+
+    PrintList(head);
+    std::cout << '\n';
+
+    QuickSort(head);
+    PrintList(head);
+}
+
